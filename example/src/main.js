5a468cd295217d86fad7b3c7ac17b6b058085c84
Saektide <real.saektide@gmail.com>
2020-04-28 01:53:37 -0300

    remove example

--- example/src/main.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue'
-import App from './App.vue'
-import './assets/app.css';
-
-Vue.config.productionTip = false
-
-new Vue({
-  render: function (h) { return h(App) },
-}).$mount('#app')
